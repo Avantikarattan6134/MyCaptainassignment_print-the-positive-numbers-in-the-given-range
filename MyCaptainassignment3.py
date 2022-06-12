89506b740716da16a6f97afaa7b50fb4983dbd65
Avantika Rattan <107038031+Avantikarattan6134@users.noreply.github.com>
2022-06-12 13:33:39 +0530

    MyCaptainassignment3
    Creating a Python program to print all positive numbers in a range.

---
+++ MyCaptainassignment3.py
@@ -0,0 +1,12 @@
+list1 = [12,-7,5,64,-14]
+for i in range (len(list1)):
+    if list1[i] > 0:
+        print(list1[i] ,end = ",")
+print()
+list2 = [12,14,-95,3]
+list3=[]
+for j in range (len(list2)):
+    if list2[j]>0:
+        a=list2[j]
+        list3.append(a)
+print (list3)
